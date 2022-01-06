25425d7f2e244f072da211967ac21b213b742389
Nourankh20 <82681541+Nourankh20@users.noreply.github.com>
2022-01-06 15:01:00 +0200

    the creatertransfer method

---
+++ server/src/modules/external/external.controller.ts
@@ -0,0 +1,16 @@
+// import { Controller, Get, Param, Post,} from "@nestjs/common";
+// // import { ExternalService } from "./external.service";
+
+
+// @Controller("external")
+// export class ExternalController {
+//   constructor(private externalService: ExternalService) {}
+ 
+
+//   @Post("/transfer")
+//   CreateAccount(@Body body:): any {
+//     const account = this.accountService.createAccount(userid);
+//     return account;
+//   }
+
+// }
--- server/src/modules/external/external.service.ts
+++ server/src/modules/external/external.service.ts
@@ -1,15 +1,19 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Options } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
-// import { Account, AccountDocument,Transaction,} from "@sp/schemas";
+import { Account, AccountDocument,Transaction,} from "@sp/schemas";
 import { Model } from "mongoose";
 import { TransactionService } from "../transaction/transaction.service";
+import { Response as Res, Request as Req } from "express";
+import { JwtService } from "@nestjs/jwt";
+
 
 
 @Injectable()
 export class AccountService {
   constructor(
-    // @InjectModel(Account.name) private accountModel: Model<AccountDocument>,
-    // private transactionService: TransactionService
+    @InjectModel(Account.name) private accountModel: Model<AccountDocument>,
+    private transactionService: TransactionService,
+    private jwtService: JwtService
   ) {}
 
   //method that creates the external transaction
@@ -30,5 +34,41 @@ export class AccountService {
     await ngrok.disconnect(url); // stops one
   }
 
+
+    async createTransfer(res:Res , req:Req ){
+        try{
+            // Get user input
+            const { receiverAccountNumber, amount, description} = req.body;
+
+            // Validate user input
+            if (!( receiverAccountNumber && amount && description)) {
+            res.status(400).send("Bad Request");
+            }
+        
+            // check if accountid exist
+            const account = await this.accountModel.findOne({ accountid:receiverAccountNumber });
+
+            if (!account) {
+            return res.status(400).send("account number not found");
+            }
+
+            let token;
+            if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') 
+                token = req.headers.authorization.split(' ')[1];
+            
+            this.jwtService.verify(token ,{secret:"My-Secret-Key"}), (err) => {
+            if(err)
+                res.status(401).send("Unauthorized, invalid token")
+            }
+        }
+        finally{
+            // call transaction.service and create new transaction with the input (from_to: External transfer)
+            // res.status(201).send("Transfer completed successfully")
+        }
+    
+    
+   
+
+ }
   
 }
